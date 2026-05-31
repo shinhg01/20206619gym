1c557c028bdd524d430f16b0a88ed3e76ca076bd
shinhg01 <a73391598@gmail.com>
2026-05-31 06:19:37 +0000

    주소 변경

--- js/config.js
+++ js/config.js
@@ -7,7 +7,7 @@
 const CONFIG = {
   // TODO: 여기에 Google Apps Script 배포 URL을 입력하세요.
   // 예: 'https://script.google.com/macros/s/AKfycb.../exec'
-  GAS_URL: 'https://script.google.com/macros/s/AKfycbyAM1qyABwnfJ7Yw2JRQRP0FTQ4xFbAib9Sxd33BsMn5vsEZOVC5Mavh570f4PF0WOw4A/exec',
+  GAS_URL: 'https://script.google.com/macros/s/AKfycbzEoHiZ0yzuX-ophI6jenymxuA7_9DsNs648lD126jYNdgFVP8ovmffS0eQK-20SnSdPg/exec',
   
   // 세션 스토리지용 키 정의
   SESSION_KEYS: {
